5c169bff8b9d02163716b34e8a6a101761629476
MarvinElz <MarvinElz@gmx.de>
2017-03-07 12:33:39 +0100

    minor changes

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -143,6 +143,7 @@ bool propagate( std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& re
 
 	ros::Time now = ros::Time::now();
   double dt = (now - last_Prop).toSec();  
+	ROS_INFO("dt: %f", dt); 
 	//dt = 0.005;						// konstante Schrittweite 
  	last_Prop = now;	
 
